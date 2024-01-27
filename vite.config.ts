ec762c7e9b8950819f4f82699ffd03bbd1f3ee5d
sarsidh <ssidharth@korewireless.com>
2024-01-27 21:04:07 +0530

    gh page config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todoApp/', // Change this to your actual repository name
   plugins: [react()],
   build: {
     outDir: 'build',
   },
-})
+});
